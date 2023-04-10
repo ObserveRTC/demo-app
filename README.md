72e9d87982bae96c615d1a4ddb4a285a2ead81f8
Balazs Kreith <kreith.balazs@gmail.com>
2023-04-10 18:31:46 +0300

    update

---
+++ README.md
@@ -0,0 +1,41 @@
+ObserveRTC Demo Application
+---
+
+## Clone
+
+```bash
+git clone https://github.com/ObserveRTC/demo-app.git
+```
+
+## Install and Build
+
+1. Install the server 
+
+```bash
+cd server && yarn
+```
+
+2. Install the webapp
+
+```bash
+cd webapp && yarn
+```
+
+## Run
+
+1. Start server
+
+```bash
+cd server
+
+node dist/main.js
+```
+
+2. Start the webapp
+
+```bash
+cd webapp
+
+yarn start
+```
+
--- server/src/monitor.ts
+++ server/src/monitor.ts
@@ -1,4 +1,4 @@
-import { EvaluatorProcess, SfuEventReport } from "@observertc/observer-js";
+import { CallEventReport, EvaluatorProcess, SfuEventReport } from "@observertc/observer-js";
 import Prometheus from 'prom-client';
 import { createLogger } from "./logger";
 
@@ -19,6 +19,10 @@ export function createMonitorProcess(registry: Prometheus.Registry): EvaluatorPr
 		help: 'The percentage of client using TURN',
 	});
 
+	const callSummaries = new Map<string, {
+		maxParticipants: number,
+	}>();
+
 	return async (context) => {
 		const { 
 			observedCalls,
@@ -28,29 +32,33 @@ export function createMonitorProcess(registry: Prometheus.Registry): EvaluatorPr
 			storages
 		} = context;
 		
-		// const calls = Array.from(observedCalls.observedCalls());
-		// const clients = Array.from(calls.flatMap(call => Array.from(call.observedClients())));
-		// const peerConnections = Array.from(clients.flatMap(peerConnection => Array.from(peerConnection.observedPeerConnections())));
-		// const inboundAudioTracks = Array.from(peerConnections.flatMap(peerConnection => Array.from(peerConnection.inboundAudioTracks())));
-		// const inboundVideoTracks = Array.from(peerConnections.flatMap(peerConnection => Array.from(peerConnection.inboundVideoTracks())));
-		// const outboundAudioTracks = Array.from(peerConnections.flatMap(peerConnection => Array.from(peerConnection.outboundAudioTracks())));
-		// const outboundVideoTracks = Array.from(peerConnections.flatMap(peerConnection => Array.from(peerConnection.outboundVideoTracks())));
-
-		// console.warn("calls", calls);
-		// console.warn("clients", clients);
-		// console.warn("peerConnections", peerConnections);
-		// console.warn("inboundAudioTracks", inboundAudioTracks);
-		// console.warn("inboundVideoTracks", inboundVideoTracks);
-		// console.warn("outboundAudioTracks", outboundAudioTracks);
-		// console.warn("outboundVideoTracks", outboundVideoTracks);
-
-		// Observe call durations
+		// Observe call durations, and reports call summaries
 		for (const endedCall of endedCalls) {
 			const elapsedTimeInMins = (endedCall.ended -  Number(endedCall.started)) / (60 * 1000);
 			callDurations.observe(elapsedTimeInMins);
+
+			if (!endedCall.callId) {
+				continue;
+			}
+			const callSummary = callSummaries.get(endedCall.callId);
+			if (!callSummary) {
+				continue;
+			}
+
+			const callSummaryReport: CallEventReport = {
+				serviceId: endedCall.serviceId ?? 'myServiceId',
+				name: 'CALL_SUMMARY_REPORT',
+				timestamp: Date.now(),
+				callId: endedCall.callId,
+				roomId: endedCall.roomId,
+				attachments: JSON.stringify(callSummary),
+			}
+			
+			reports.addCallEventReport(callSummaryReport);
+			callSummaries.delete(endedCall.callId);
 		}
 
-		// how many clients using turn
+		// Observe how many clients using turn
 		const { peerConnectionStorage, clientStorage } = storages;
 		const clientsUsingTurn = new Set<string>();
 		for await (const [peerConnectionId, peerConnection] of peerConnectionStorage) {
@@ -66,14 +74,13 @@ export function createMonitorProcess(registry: Prometheus.Registry): EvaluatorPr
 		turnUsage.set((clientsUsingTurn.size / numberOfClients) * 100)
 
 
-		// save sfu load at a time by creating a new report
-		const sfuUsageReport: SfuEventReport = {
-			serviceId: 'myServiceId',
-			name: 'SFU_LOAD_REPORT',
-			timestamp: Date.now(),
-
+		// Iterate calls and update call summaries
+		const { callStorage } = storages;
+		for await (const [callId, call] of callStorage) {
+			const callSummary = callSummaries.get(callId);
+			callSummaries.set(callId, {
+				maxParticipants: Math.max(callSummary?.maxParticipants ?? 0, call.clientIds.length),
+			});
 		}
-		reports.addSfuEventReport(sfuUsageReport);
-		
 	}
 }
